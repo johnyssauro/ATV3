61bc532495573b746233f6de731ddf3406a87c5a
João <joaopedronoronhadutra@gmail.com>
2024-03-22 10:28:23 -0300

    Remoção da função acertarRotas

--- analisarRotas.h
+++ analisarRotas.h
@@ -8,20 +8,12 @@ typedef struct rota
 
 };
 
-/* Criando função acertarRotas
-
-    @param noInicial Corresponde ao nó de partida da entrega (em coordenadas)
-    @param noFinal Corresponde ao nó final da entrega (em coordenadas)
-    @return Retorna 0 se noInicial == noFinal. Retorna 1 se noInicial != noFinal
-*/
-int acertarRotas(double noInicial, double noFinal, double variaveis);
-
 /* Criando função analisarRotas
 
     @param noInicial Corresponde ao nó de partida da entrega (em coordenadas)
     @param noFinal Corresponde ao nó final da entrega (em coordenadas)
     @param variaveis Corresponde às variáveis que definem a rota entre os dois nós
     @param r Ponteiro para o endereço de uma lista de nós que ligam noInicial e noFinal 
-    @return Retorna r com sua lista de nós;
+    @return Retorna r com sua lista de nós. Retorna NULL se noInicial == noFinal;
 */
 int analisarRotas(double noInicial, double noFinal, double variaveis, double r);
